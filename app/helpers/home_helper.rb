3576cbe2cc12579e7fc311a2892a7e48d222fb72
Davo Davosky <davodavosky@gmail.com>
2025-01-06 17:01:49 +0100

    January view data and chart on subsctiption types section

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -53,4 +53,23 @@ module HomeHelper
     cur_tot = instance_variable_get("@from_previous_year_#{month}").regional.to_f
     percentual = ((cur_ran / cur_tot) * 100).round(2)
   end
+
+  #Returns the value of the subscription types from the giving year per month to put on chartkick chart
+  def chart_kick_subscription_type_percentual(year, month, subscription_type)
+    par_sub = instance_variable_get("@from_#{year}_year_#{month}").send(subscription_type).to_f
+    tot_sub = instance_variable_get("@from_#{year}_year_#{month}").regional.to_f
+    percentual = ((par_sub / tot_sub) * 100).round(2)
+  end
+
+  def growth_degrowth_subscription_type(month, subscription_type)
+    pre_value = instance_variable_get("@from_previous_year_#{month}").send(subscription_type).to_f
+    cur_value = instance_variable_get("@from_current_year_#{month}").send(subscription_type).to_f
+    percentual = ((cur_value - pre_value) / pre_value) * 100
+
+    if cur_value > pre_value
+      "<span class='text-success fs-6'><i class='fa-solid fa-circle-arrow-up'></i>&nbsp;#{percentual.round(2)}%</span>".html_safe
+    else
+      "<span class='text-danger fs-6'><i class='fa-solid fa-circle-arrow-down'></i>&nbsp;#{percentual.round(2)}%</span>".html_safe
+    end
+  end
 end
